a69f6571987bb5d60ba6a4d4ade92c1c6217e4a8
krugerrgabriel <gabriel.sa.kruger@gmail.com>
2022-09-01 13:56:09 -0300

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
 import NextNProgress from 'nextjs-progressbar';
+
+import * as gtag from '../lib/gtag';
+import Analytics from '../components/Analytics';
 
 import GlobalStyle from '../styles/global';
 import { ThemeProvider } from 'styled-components';
@@ -8,13 +13,27 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 
 import 'bootstrap/dist/css/bootstrap.css';
+
 // @ts-ignore
-const MyApp: React.FC<AppProps> = ({ Component, pageProps, carros }) => {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
       <NextNProgress color="#971915" />
       <Component {...pageProps} />
       <GlobalStyle />
+      <Analytics />
     </ThemeProvider>
   );
 };
