0abda91aeeb0b734b66fa77cbd2433b1f6fbc963
krugerrgabriel <gabriel.sa.kruger@gmail.com>
2022-09-23 15:57:01 -0300

    Next Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
   siteUrl: 'https://www.tkars.com.br',
   generateRobotsTxt: true,
   changefreq: 'daily',
+  generateIndexSitemap: false,
   priority: 0.8
   // ...other options
 };
